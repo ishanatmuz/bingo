c96c95ac6950ad650f1ef7793d5f5e60606807e5
Ishan <ishanatmuzaffarpur@gmail.com>
2017-05-11 03:19:14 +0530

    draw grid, numbers and selections

--- static/js/renderer.js
+++ static/js/renderer.js
@@ -96,7 +96,20 @@ var Renderer = {
     },
     
     drawCanvas: function(data) {
+        // Draw grid
         this.drawGrid(data);
+        // Draw board
+        for (var i = 0; i < data.numRows; i++) {
+            for (var j = 0; j < data.numRows; j++) {
+                var number = data.board[i][j];
+                // Draw number
+                this.drawNumber(data, number, i, j);
+                // Draw selection cross
+                if (data.selections.indexOf(number) !== -1) {
+                    this.drawCross(data, i, j);
+                }
+            }
+        }
     },
     
     drawGrid: function(data) {
@@ -116,7 +129,27 @@ var Renderer = {
         }
     },
     
-    drawNumber: function(data, number, position) {
+    drawNumber: function(data, number, x, y) {
+        data.ctx.font = '40px serif';
+        var xOffset = 10;
+        if (number < 10) {
+            xOffset = 20;
+        }
+        data.ctx.fillText('' + number, (y * 60) + xOffset, ((x + 1) * 60) - 15);
+    },
+    
+    drawCross: function(data, x, y) {
+        var width = (300 / data.numRows);
+        // Diagonal 1
+        data.ctx.beginPath();
+        data.ctx.moveTo(width * y, width * x);
+        data.ctx.lineTo(width * (y + 1), width * (x + 1));
+        data.ctx.stroke();
         
+        // Diagonal 2
+        data.ctx.beginPath();
+        data.ctx.moveTo(width * y, width * (x + 1));
+        data.ctx.lineTo(width * (y + 1), width * x);
+        data.ctx.stroke();
     }
 };
