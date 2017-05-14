17ce42a70975b17f2484988b1687009b6960be37
Ishan <ishanatmuzaffarpur@gmail.com>
2017-05-14 19:13:26 +0530

    xpad input selection dialog added

--- static/js/help.js
+++ static/js/help.js
@@ -6,11 +6,24 @@ var Help = {
 				// Displaying the help dialog
 				alertify.okBtn("Close").alert(_.template(document.getElementById('helpTmpl').innerHTML));
 			});
+			
 			var xpadIcon = document.getElementById('xpad-icon');
 			xpadIcon.addEventListener('click', function() {
 				// Displayig the gamepad dialog
 				alertify.okBtn("Close").alert(_.template(document.getElementById('xpadTmpl').innerHTML));
 			});
+		}
+	},
+
+	displayXpadInput: function(buttonType) {
+		// Display the button type 
+		var xpadSelectedInput = document.getElementById('xpad-selected-input');
+		if (!_.isNull(xpadSelectedInput)) {
+			if (_.contains(['A', 'X', 'Menu', 'D-Up', 'D-Down', 'D-Left', 'D-Right'], buttonType)) {
+				xpadSelectedInput.innerText = buttonType;
+			} else {
+				xpadSelectedInput.innerText = 'Press any of the above mentioned buttons.';
+			}
 		}
 	}
 };
